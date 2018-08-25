ace09a3f1d4000e015ab9e725e358a1be2d79890
clwilkening <clw13clw13@gmail.com>
2018-08-25 08:44:06 -0400

    first template

--- solutions-cw/routes/index.js
+++ solutions-cw/routes/index.js
@@ -3,7 +3,20 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  //res.send('Hey! It works!');
+  //res.send(req.query.name ? req.query.name : 'no name man');
+  //res.json(req.query);
+  res.render('hello', {
+    name: 'wes',
+    dog: req.query.dog
+  });
+
 });
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
+});
+
 
 module.exports = router;
