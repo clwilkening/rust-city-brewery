d812bc09e075cb421942ce0cf829f25485ba6d0c
clwilkening <clw13clw13@gmail.com>
2018-08-27 13:54:33 -0400

    add homePage router

--- solutions-cw/routes/index.js
+++ solutions-cw/routes/index.js
@@ -1,23 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
 // Do work here
-router.get('/', (req, res) => {
-  //res.send('Hey! It works!');
-  //res.send(req.query.name ? req.query.name : 'no name man');
-  //res.json(req.query);
-  res.render('hello', {
-    name: 'wes',
-    dog: req.query.dog,
-    title: 'I love food.'
-  });
+router.get('/', storeController.homePage);
 
-});
-
-router.get('/reverse/:name', (req, res) => {
-  const reverse = [...req.params.name].reverse().join('');
-  res.send(reverse);
-});
+// router.get('/reverse/:name', (req, res) => {
+//   const reverse = [...req.params.name].reverse().join('');
+//   res.send(reverse);
+// });
 
 
 module.exports = router;
