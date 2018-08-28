ad192842bcdb7a1b743855772469dc2866f693ea
clwilkening <clw13clw13@gmail.com>
2018-08-28 06:36:45 -0400

    08 add middleware

--- solutions-cw/controllers/storeController.js
+++ solutions-cw/controllers/storeController.js
@@ -1,3 +1,11 @@
+exports.myMiddleware = (req,res,next) => {
+  req.name = 'Chris';
+  if (req.name === 'Chris') {
+    throw Error('That is a stupid name');
+  }
+  next();
+}
 exports.homePage = (req, res) => {
+  console.log(req.name);
   res.render('index');
 };
