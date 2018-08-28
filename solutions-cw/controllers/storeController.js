40bd21aafc563149bf4b8b133798308cafb2095c
clwilkening <clw13clw13@gmail.com>
2018-08-28 07:21:23 -0400

    add store model

--- solutions-cw/controllers/storeController.js
+++ solutions-cw/controllers/storeController.js
@@ -1,10 +1,3 @@
-exports.myMiddleware = (req,res,next) => {
-  req.name = 'Chris';
-  if (req.name === 'Chris') {
-    throw Error('That is a stupid name');
-  }
-  next();
-}
 exports.homePage = (req, res) => {
   console.log(req.name);
   res.render('index');
---
+++ solutions-cw/models/Store.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise; // set the mongoose promise equal to global node.JS Promise
+const slug = require('slugs');
+
+const storeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim:true,
+    required: 'Please enter a store name!'
+  },
+  slug: String,
+  description: {
+    type: String,
+    trim: true
+  },
+  tags: [String]
+});
+
+// not an arrow => because we need 'this'
+storeSchema.pre('save', function(next) {
+  // this. current instance of store we are saving
+  if (!this.isModified('name')) {
+    return next(); // skip it
+  }
+  this.slug = slug(this.name);
+  next();
+  // TODO make more resiliant so slugs are unique
+});
+
+module.exports = mongoose.model('Store', storeSchema);
