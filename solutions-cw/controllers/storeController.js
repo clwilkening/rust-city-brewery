525cbee47f8aced644ccc34742de72f4600c354b
clwilkening <clw13clw13@gmail.com>
2018-09-04 08:58:15 -0400

    edit functionality

--- solutions-cw/controllers/storeController.js
+++ solutions-cw/controllers/storeController.js
@@ -22,3 +22,25 @@ exports.getStores =  async (req, res) => {
   const stores = await Store.find();
   res.render('stores', { title: 'Stores', stores });
 };
+
+exports.editStore = async (req, res) => {
+  // find the store given the id
+  const store = await Store.findOne({ _id: req.params.id });
+ 
+  // confirm they are the owner of the store
+  // TODO
+  // render out the edit form so user cna update
+  res.render('editStore', {title: `Edit ${store.name}`, store });
+};
+
+exports.updateStore = async (req, res) => {
+  //find and update the store
+  const store = await Store.findOneAndUpdate({_id: req.params.id}, req.body, 
+    {
+    new:true, // return the new store instead of old)
+    runValidators: true,
+  }).exec();
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store</a>`);
+  //redirect them to store and tell them it worked
+  res.redirect(`/stores/${store._id}/edit`);
+};
