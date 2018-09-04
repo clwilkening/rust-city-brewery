43558ef5da84fa6c81289a7050d8b4e2da775ed2
clwilkening <clw13clw13@gmail.com>
2018-09-04 07:15:36 -0400

    add query db for stores

--- solutions-cw/controllers/storeController.js
+++ solutions-cw/controllers/storeController.js
@@ -6,13 +6,19 @@ exports.homePage = (req, res) => {
   res.render('index');
 };
 
-exports.addStore = (req,res) => {
+exports.addStore = (req, res) => {
   res.render('editStore', { title:'Add Store' })
-}
+};
 
-exports.createStore = async (req,res) => {
+exports.createStore = async (req, res) => {
   //res.json(req.body)
   const store = await (new Store(req.body)).save();
   req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`)
   res.redirect(`/store/${store.slug}`);
-}
+};
+
+exports.getStores =  async (req, res) => {
+  // query the db for list of stores
+  const stores = await Store.find();
+  res.render('stores', { title: 'Stores', stores });
+};
